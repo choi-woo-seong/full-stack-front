6b59c8c340b83c3fe382906b1728a5f8ab571251
choi-woo-seong <goqus903@naver.com>
2025-03-27 12:34:51 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import React,{useState, useEffect} from 'react';
+import axios from 'axios';
 
 function App() {
+  const [currentTime, setCurrnetTime] = useState('');
+
+  useEffect(()=>{
+    axios.get('http://localhost:8080/api/time')
+    .then(response => {
+      setCurrnetTime(response.data);
+    })
+    .catch(error => {
+      console.log('API 호출중 오류 발생', error);
+    })
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>현재시간</h1>
+      <h3>{currentTime}</h3>
     </div>
   );
 }
